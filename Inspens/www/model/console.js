f2662d345349f654f2d86299a413105956ade095
Izzulmakin <makin.ugm@gmail.com>
2015-12-23 07:47:44 +0700

    ngggarai cordova error

--- Inspens/www/model/console.js
+++ Inspens/www/model/console.js
@@ -1,9 +1,11 @@
 function console_run() {
-	query = $("#console").val();
+	var query = $("#console").val();
 	db.transaction(function(tx) {
+		console.log("querying "+query);
 		tx.executeSql(
 			query,[],
 			function(tx,res){
+				console.log("DONE querying "+query);
 				var div_content = ">"
 				for (i=0;i<res.rows.length;i++) {
 					div_content = div_content + res.rows.item(i).toSource();
